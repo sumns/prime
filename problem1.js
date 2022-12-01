cf9a7de8b16923d66c81cbe4bea9e654649e9373
SumatM <sumatmallick@gmail.com>
2022-12-01 21:37:35 +0530

    Code is fixed

--- problem1.js
+++ problem1.js
@@ -1,12 +1,14 @@
 function prime(num){
 	let count=0;
 	for(let i=1; i<=num; i++){
-		if(num%i==0){	
+		if(num%i==0){
+            count++; 	
 		}
 	}
 	if(count==2){
 		console.log("Prime number);
 	}else{
-		console.log("Not prime number);
+		console.log("Non prime number);
 	}
+
 prime(13);
